5c17b1c4b76076d7b4fa7e6f892997277bfa81a8
guopei <376980440@qq.com>
2023-11-02 09:30:10 +0800

    update parameters

--- StyleTransfer/neural_style_transfer.py
+++ StyleTransfer/neural_style_transfer.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
     parser.add_argument("--height", type=int, help="height of content and style images", default=400)
 
     parser.add_argument("--content_weight", type=float, help="weight factor for content loss", default=1e5)
-    parser.add_argument("--style_weight", type=float, help="weight factor for style loss", default=0.1)
+    parser.add_argument("--style_weight", type=float, help="weight factor for style loss", default=1)
     parser.add_argument("--tv_weight", type=float, help="weight factor for total variation loss", default=1)
 
     parser.add_argument("--optimizer", type=str, choices=['lbfgs', 'adam'], default='adam')
